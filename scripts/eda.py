cde3c5ff83eeeda425a532056e3d754f03481efe
Aleksey Senkin <alexennk@yandex.by>
2024-09-16 12:08:12 +0300

    DS-1.2 EDA
    Changes:
    - minor changes in comments and description
    - performed graph plotting and features extraction over the aggregated dataset instead of the original one
    - changed a little some functions in the eda script

--- scripts/eda.py
+++ scripts/eda.py
@@ -43,13 +43,13 @@ def test_add_lag_features(df, original_df, lags=3):
     df.fillna(0, inplace=True)
     return df
 
-def threshold_sales(df, aggregated_df, column_name):
-    column_aggregated = df.groupby([column_name])['item_cnt_day'].sum()
+def threshold_sales(df, aggregated_df, column_name, on_test=False):
+    column_aggregated = df.groupby([column_name])['item_cnt_month'].mean()
     column_aggregated = pd.DataFrame(column_aggregated).reset_index()
 
     def divide_sales(sales):
-        low_threshold = column_aggregated['item_cnt_day'].quantile(0.25)
-        high_threshold = column_aggregated['item_cnt_day'].quantile(0.75)
+        low_threshold = column_aggregated['item_cnt_month'].quantile(0.25)
+        high_threshold = column_aggregated['item_cnt_month'].quantile(0.75)
 
         if sales < low_threshold:
             return 0
@@ -58,9 +58,13 @@ def threshold_sales(df, aggregated_df, column_name):
         else:
             return 2
         
-    column_aggregated[column_name + '_sales_level'] = column_aggregated['item_cnt_day'].apply(divide_sales)
+    column_aggregated[column_name + '_sales_level'] = column_aggregated['item_cnt_month'].apply(divide_sales)
 
     aggregated_df = aggregated_df.merge(column_aggregated, on=column_name, how='left')
-    aggregated_df.drop('item_cnt_day', axis=1, inplace=True)
+    if on_test:
+        aggregated_df.drop('item_cnt_month', axis=1, inplace=True)
+    else:
+        aggregated_df.drop('item_cnt_month_y', axis=1, inplace=True)
+        aggregated_df.rename(columns={'item_cnt_month_x': 'item_cnt_month'}, inplace=True)
 
     return aggregated_df
