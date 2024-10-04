0ddd6a2e5cb30dd1cef7078dc4a56b3f330e3ec5
Aleksey Senkin <alexennk@yandex.by>
2024-10-04 12:54:45 +0300

    DS-2.1 Baseline modeling pt.1:
    Changes:
    - fixed "months_since_last_sale" calculation mistake
    - added "months_since_last_sale" feature to the test dataset
    - added "avg_item_price" lag feature

--- scripts/feature_extr.py
+++ scripts/feature_extr.py
@@ -13,27 +13,29 @@ def transform_df_types(df, int_columns, float_columns=None, object_columns=None,
     return df
 
 
-def train_add_lag_features(df, col, on=['item_id', 'shop_id', 'date_block_num'], lags=[1, 2, 3]):
-    new_df = df[['item_id', 'shop_id', 'date_block_num', col]]
+def train_add_lag_features(df, col, on_columns=['date_block_num', 'shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
+    new_df = df[on_columns + [col]]
+    new_df = new_df.groupby(on_columns).agg({col: operation}).reset_index()
     for lag in lags:
         tmp = new_df.copy()
         tmp.loc[:, 'date_block_num'] = tmp['date_block_num'] + lag
-        tmp.columns = ['item_id', 'shop_id', 'date_block_num', col + '_lag_' + str(lag)]
-        df = df.merge(tmp, on=['item_id', 'shop_id', 'date_block_num'], how='left')
+        tmp.columns = on_columns + [col + '_lag_' + str(lag)]
+        df = df.merge(tmp, on=on_columns, how='left')
 
     for lag in lags:
         df['{}_lag_{}'.format(col, lag)] = df['{}_lag_{}'.format(col, lag)].fillna(0)
     return df
 
 
-def test_add_lag_features(df, original_df, col, on=['item_id', 'shop_id'], lags=[1, 2, 3]):
-    new_df = original_df[['item_id', 'shop_id', 'date_block_num', col]]
+def test_add_lag_features(df, original_df, col, on_columns=['shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
+    new_df = original_df[on_columns + ['date_block_num', col]]
+    new_df = new_df.groupby(on_columns + ['date_block_num']).agg({col: operation}).reset_index()
     for lag in lags:
         tmp = new_df.copy()
         tmp = tmp[tmp['date_block_num'] == 34 - lag]
         tmp.drop('date_block_num', axis=1, inplace=True)
-        tmp.columns = ['item_id', 'shop_id', col + '_lag_' + str(lag)]
-        df = df.merge(tmp, on=['item_id', 'shop_id'], how='left')
+        tmp.columns = on_columns + [col + '_lag_' + str(lag)]
+        df = df.merge(tmp, on=on_columns, how='left')
 
     for lag in lags:
         df['{}_lag_{}'.format(col, lag)] = df['{}_lag_{}'.format(col, lag)].fillna(0)
