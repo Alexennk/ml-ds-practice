8edcab43a2ae261db70bbfc7f890f6cb9d417334
Aleksey Senkin <alexennk@yandex.by>
2024-10-04 15:16:55 +0300

    DS-2.1 Baseline modeling pt.1
    Changes:
    - loaded and transformed dtypes of train, test datasets in the Validation Layer

--- scripts/validate.py
+++ scripts/validate.py
@@ -2,3 +2,10 @@ import numpy as np
 import pandas as pd
 
 
+def transform_df_types(df, int_columns, float_columns=None, object_columns=None, int_type=np.int32):
+    df[int_columns] = df[int_columns].astype(int_type)
+    if float_columns is not None:
+        df[float_columns] = df[float_columns].astype(np.float32)
+    if object_columns is not None:
+        df[object_columns] = df[object_columns].astype('category')
+    return df
