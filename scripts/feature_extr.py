e7166aff292009ecfc5c9bd2bf829f0b0d32edf8
Aleksey Senkin <alexennk@yandex.by>
2024-10-06 00:30:56 +0300

    DS-2.1 Baseline modeling pt.1
    Changes:
    - added two more groups of lag features - avg sales by category and by category/shop
    - performed simple process of xgboost model validation

--- scripts/feature_extr.py
+++ scripts/feature_extr.py
@@ -1,7 +1,5 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def transform_df_types(df, int_columns, float_columns=None, object_columns=None, int_type=np.int32):
@@ -13,32 +11,32 @@ def transform_df_types(df, int_columns, float_columns=None, object_columns=None,
     return df
 
 
-def train_add_lag_features(df, col, on_columns=['date_block_num', 'shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
+def train_add_lag_features(df, col, add_name='', on_columns=['date_block_num', 'shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
     new_df = df[on_columns + [col]]
     new_df = new_df.groupby(on_columns).agg({col: operation}).reset_index()
     for lag in lags:
         tmp = new_df.copy()
         tmp.loc[:, 'date_block_num'] = tmp['date_block_num'] + lag
-        tmp.columns = on_columns + [col + '_lag_' + str(lag)]
+        tmp.columns = on_columns + [col + add_name + '_lag_' + str(lag)]
         df = df.merge(tmp, on=on_columns, how='left')
 
     for lag in lags:
-        df['{}_lag_{}'.format(col, lag)] = df['{}_lag_{}'.format(col, lag)].fillna(0)
+        df['{}_lag_{}'.format(col + add_name, lag)] = df['{}_lag_{}'.format(col + add_name, lag)].fillna(0)
     return df
 
 
-def test_add_lag_features(df, original_df, col, on_columns=['shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
+def test_add_lag_features(df, original_df, col, add_name='', on_columns=['shop_id', 'item_id'], operation='mean', lags=[1, 2, 3]):
     new_df = original_df[on_columns + ['date_block_num', col]]
     new_df = new_df.groupby(on_columns + ['date_block_num']).agg({col: operation}).reset_index()
     for lag in lags:
         tmp = new_df.copy()
         tmp = tmp[tmp['date_block_num'] == 34 - lag]
         tmp.drop('date_block_num', axis=1, inplace=True)
-        tmp.columns = on_columns + [col + '_lag_' + str(lag)]
+        tmp.columns = on_columns + [col + add_name + '_lag_' + str(lag)]
         df = df.merge(tmp, on=on_columns, how='left')
 
     for lag in lags:
-        df['{}_lag_{}'.format(col, lag)] = df['{}_lag_{}'.format(col, lag)].fillna(0)
+        df['{}_lag_{}'.format(col + add_name, lag)] = df['{}_lag_{}'.format(col + add_name, lag)].fillna(0)
     return df
 
 
