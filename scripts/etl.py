aa2c81d06f848bfbb148680842e00a520c545101
Aleksey Senkin <alexennk@yandex.by>
2024-11-17 00:23:22 +0300

    DS-2.1 Baseline modeling pt.1
    Changes:
    - edited "transform_df_types()" function

--- scripts/etl.py
+++ scripts/etl.py
@@ -4,12 +4,14 @@ import itertools
 
 
 # I use this function in every single notebook, so it's easier to import it as a separate function
-def transform_df_types(df, int_columns, float_columns=None, object_columns=None):
-    df[int_columns] = df[int_columns].astype(np.int32)
-    if float_columns is not None:
-        df[float_columns] = df[float_columns].astype(np.float32)
-    if object_columns is not None:
-        df[object_columns] = df[object_columns].astype('category')
+def transform_df_types(df, transform_date=False, float_type=np.float32):
+    if transform_date:
+        df['date'] = pd.to_datetime(df['date'], format='%d.%m.%Y')
+    
+    float_columns = df.select_dtypes(include=np.number).columns.tolist()
+    object_columns = df.select_dtypes(include=object).columns.tolist()
+    df[float_columns] = df[float_columns].astype(float_type)
+    df[object_columns] = df[object_columns].astype('category')
     return df
 
 
