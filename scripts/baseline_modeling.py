2311bd05a2667e53efa77435e6cf771d353f2f72
Aleksey Senkin <alexennk@yandex.by>
2024-11-23 17:11:00 +0300

    DS-2.2 Baseline modeling pt.2
    Changes:
    - added flake8 and black to pre-commit hooks

--- scripts/baseline_modeling.py
+++ scripts/baseline_modeling.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import BaseCrossValidator
 from xgboost import XGBRegressor
@@ -17,7 +16,9 @@ class TimeSeriesSplit(BaseCrossValidator):
         random_state: random seed
     """
 
-    def __init__(self, n_splits=5, method='expanding', random_state=None, train_start=0):
+    def __init__(
+        self, n_splits=5, method="expanding", random_state=None, train_start=0
+    ):
         self.n_splits = n_splits
         self.method = method
         self.train_start = train_start
@@ -27,43 +28,56 @@ class TimeSeriesSplit(BaseCrossValidator):
         return self.n_splits
 
     def split(self, X, y=None, groups=None):
-        n_months = X['date_block_num'].max() + 1
+        n_months = X["date_block_num"].max() + 1
         method = self.method
-        
-        if method == 'expanding':
+
+        if method == "expanding":
 
             for i in range(self.n_splits, 0, -1):
-                train_idx = (X['date_block_num'] < n_months - i) & (X['date_block_num'] >= self.train_start)
-                test_idx = X['date_block_num'] == n_months - i
+                train_idx = (X["date_block_num"] < n_months - i) & (
+                    X["date_block_num"] >= self.train_start
+                )
+                test_idx = X["date_block_num"] == n_months - i
 
                 yield train_idx, test_idx
-        
-        elif method == 'sliding':
 
-            m_in_split = n_months // self.n_splits # number of months in a single split (perhaps, except for the last one)
+        elif method == "sliding":
+            # number of months in a single split (perhaps, except for the last one)
+            m_in_split = n_months // self.n_splits
 
             for i in range(1, self.n_splits):
-                train_idx = (X['date_block_num'] < i * m_in_split) & (X['date_block_num'] >= m_in_split * (i - 1))
-                test_idx = X['date_block_num'] == i * m_in_split
+                train_idx = (X["date_block_num"] < i * m_in_split) & (
+                    X["date_block_num"] >= m_in_split * (i - 1)
+                )
+                test_idx = X["date_block_num"] == i * m_in_split
 
                 yield train_idx, test_idx
-            
-            train_idx = (X['date_block_num'] < n_months - 1) & (X['date_block_num'] >= m_in_split * (self.n_splits - 1)) # all indexes left go to the last block
-            test_idx = X['date_block_num'] == n_months - 1
+
+            # all indexes left go to the last block
+            train_idx = (X["date_block_num"] < n_months - 1) & (
+                X["date_block_num"] >= m_in_split * (self.n_splits - 1)
+            )
+            test_idx = X["date_block_num"] == n_months - 1
 
             yield train_idx, test_idx
 
         else:
-            
+
             raise ValueError("'method' parameter should be 'expanding' or 'sliding'")
 
 
 class TrainModels:
     @staticmethod
-    def train_linear_regression(X, y, cv_method='expanding', return_scores=False, return_model=False, cv_n_splits=5):
-        
+    def train_linear_regression(
+        X,
+        y,
+        cv_method="expanding",
+        return_scores=False,
+        return_model=False,
+        cv_n_splits=5,
+    ):
         """
-        Returns on of these: 
+        Returns on of these:
             - None
             - model
             - scores
@@ -75,10 +89,10 @@ class TrainModels:
 
         for train_idx, test_idx in tscv.split(X):
             X_new = X.copy()
-            X_new.drop('date_block_num', axis=1, inplace=True)
+            X_new.drop("date_block_num", axis=1, inplace=True)
             X_train, X_test = X_new[train_idx], X_new[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
-            
+
             model = LinearRegression()
 
             model.fit(X_train, y_train)
@@ -88,22 +102,35 @@ class TrainModels:
             scores.append(score)
 
             print(f"{len(scores)} split RMSE: {score:.2f}\n")
-        
+
         print(f"Average RMSE: {np.mean(scores):.2f}")
 
         if return_scores or return_model:
             to_return = []
-            if return_model: to_return.append(model)
-            if return_scores: to_return.append(scores)
+            if return_model:
+                to_return.append(model)
+            if return_scores:
+                to_return.append(scores)
             return tuple(to_return)
 
-
     @staticmethod
-    def train_xgboost(X, y, cv_method='expanding', return_scores=False, return_model=False, verbose=False, cv_n_splits=5,
-                    n_estimators=1000, max_depth=7, learning_rate=0.05, early_stopping_rounds=30, subsample=0.8, colsample_bytree=0.8):
-        
+    def train_xgboost(
+        X,
+        y,
+        cv_method="expanding",
+        return_scores=False,
+        return_model=False,
+        verbose=False,
+        cv_n_splits=5,
+        n_estimators=1000,
+        max_depth=7,
+        learning_rate=0.05,
+        early_stopping_rounds=30,
+        subsample=0.8,
+        colsample_bytree=0.8,
+    ):
         """
-        Returns on of these: 
+        Returns on of these:
             - None
             - model
             - scores
@@ -115,33 +142,40 @@ class TrainModels:
 
         for train_idx, test_idx in tscv.split(X):
             X_new = X.copy()
-            X_new.drop('date_block_num', axis=1, inplace=True)
+            X_new.drop("date_block_num", axis=1, inplace=True)
             X_train, X_test = X_new[train_idx], X_new[test_idx]
             y_train, y_test = y[train_idx], y[test_idx]
-            
+
             model = XGBRegressor(
                 max_depth=max_depth,
                 n_estimators=n_estimators,
-                learning_rate=learning_rate,    
+                learning_rate=learning_rate,
                 eval_metric="rmse",
                 early_stopping_rounds=early_stopping_rounds,
                 subsample=subsample,
                 colsample_bytree=colsample_bytree,
-                random_state=42
+                random_state=42,
             )
 
-            model.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], verbose=verbose)
+            model.fit(
+                X_train,
+                y_train,
+                eval_set=[(X_train, y_train), (X_test, y_test)],
+                verbose=verbose,
+            )
 
             y_pred = model.predict(X_test)
             score = np.sqrt(mean_squared_error(y_test, y_pred))
             scores.append(score)
 
             print(f"{len(scores)} split RMSE: {score:.2f}\n")
-        
+
         print(f"Average RMSE: {np.mean(scores):.2f}")
 
         if return_scores or return_model:
             to_return = []
-            if return_model: to_return.append(model)
-            if return_scores: to_return.append(scores)
+            if return_model:
+                to_return.append(model)
+            if return_scores:
+                to_return.append(scores)
             return tuple(to_return)
--- scripts/eda.py
+++ scripts/eda.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,32 +5,47 @@ import seaborn as sns
 
 def plot_barpot_boxplot(df, column_name, figsize=(14, 7), leave=None):
     # First Plot Data
-    column_aggregated = df.groupby([column_name], observed=False)['item_cnt_month'].mean()
+    column_aggregated = df.groupby([column_name], observed=False)[
+        "item_cnt_month"
+    ].mean()
     column_aggregated = pd.DataFrame(column_aggregated)
     column_aggregated.reset_index(inplace=True)
-    column_aggregated = column_aggregated.sort_values(['item_cnt_month'], ascending=False)
+    column_aggregated = column_aggregated.sort_values(
+        ["item_cnt_month"], ascending=False
+    )
     if leave is not None:
         column_aggregated = column_aggregated[:leave]
 
     # Second Plot Data
     var = column_name
-    data = pd.concat([df['item_cnt_month'], df[var]], axis=1)
+    data = pd.concat([df["item_cnt_month"], df[var]], axis=1)
 
     # Create subplots in one row with two columns
-    fig, axes = plt.subplots(1, 2, figsize=figsize)  # Adjust the figsize as needed for better appearance
+    fig, axes = plt.subplots(
+        1, 2, figsize=figsize
+    )  # Adjust the figsize as needed for better appearance
 
     # First plot: Barplot
-    sns.barplot(x=column_aggregated['item_cnt_month'], 
-                y=column_aggregated[column_name], 
-                orient='h', 
-                order=column_aggregated[column_name], 
-                ax=axes[0])
+    sns.barplot(
+        x=column_aggregated["item_cnt_month"],
+        y=column_aggregated[column_name],
+        orient="h",
+        order=column_aggregated[column_name],
+        ax=axes[0],
+    )
     axes[0].set_title(column_name + " Sales Comparison")
     axes[0].set_xlabel("Average Sales Amount")
     axes[0].set_ylabel(column_name)
 
     # Second plot: Boxplot
-    sns.boxplot(x='item_cnt_month', y=var, data=data, orient='h', order=column_aggregated[column_name], ax=axes[1])
+    sns.boxplot(
+        x="item_cnt_month",
+        y=var,
+        data=data,
+        orient="h",
+        order=column_aggregated[column_name],
+        ax=axes[1],
+    )
     axes[1].set_title("Box Plots for " + column_name)
     axes[1].set_xlabel("Item Count per Month")
     axes[1].set_ylabel("")
@@ -41,27 +55,53 @@ def plot_barpot_boxplot(df, column_name, figsize=(14, 7), leave=None):
     plt.show()
 
 
-def plot_feature_comparison(df, feature_to_compare, feature_to_aggregate, top_objects=None, left_lim_objects=0, right_lim_objects=5, agg_operation='sum', limit_city=None, fig_size=(6, 4), legend_size=8, legend_loc=2, ylim=None):
+def plot_feature_comparison(
+    df,
+    feature_to_compare,
+    feature_to_aggregate,
+    top_objects=None,
+    left_lim_objects=0,
+    right_lim_objects=5,
+    agg_operation="sum",
+    limit_city=None,
+    fig_size=(6, 4),
+    legend_size=8,
+    legend_loc=2,
+    ylim=None,
+):
     if top_objects is None:
-        column_aggregated = df.groupby([feature_to_aggregate], observed=False)[feature_to_compare].sum()
+        column_aggregated = df.groupby([feature_to_aggregate], observed=False)[
+            feature_to_compare
+        ].sum()
         column_aggregated = pd.DataFrame(column_aggregated)
         column_aggregated.reset_index(inplace=True)
-        column_aggregated = column_aggregated.sort_values([feature_to_compare], ascending=False)
+        column_aggregated = column_aggregated.sort_values(
+            [feature_to_compare], ascending=False
+        )
 
-        top_objects = column_aggregated[feature_to_aggregate][left_lim_objects:right_lim_objects].values
+        top_objects = column_aggregated[feature_to_aggregate][
+            left_lim_objects:right_lim_objects
+        ].values
 
     if limit_city is not None:
-        df = df[df['shop_city'] == limit_city]
+        df = df[df["shop_city"] == limit_city]
     column_month_aggregated = df[df[feature_to_aggregate].isin(top_objects)]
-    column_month_aggregated = column_month_aggregated.groupby([feature_to_aggregate, 'date_block_num'], observed=False)[feature_to_compare].agg(agg_operation)
+    column_month_aggregated = column_month_aggregated.groupby(
+        [feature_to_aggregate, "date_block_num"], observed=False
+    )[feature_to_compare].agg(agg_operation)
     column_month_aggregated = pd.DataFrame(column_month_aggregated)
     column_month_aggregated.reset_index(inplace=True)
 
     plt.figure(figsize=fig_size)
-    ax = sns.lineplot(x='date_block_num', y=feature_to_compare, hue=feature_to_aggregate, data=column_month_aggregated)
+    sns.lineplot(
+        x="date_block_num",
+        y=feature_to_compare,
+        hue=feature_to_aggregate,
+        data=column_month_aggregated,
+    )
     plt.title(feature_to_compare + " Comparison")
     if ylim is not None:
         plt.ylim(0, ylim)
-    plt.legend(loc=legend_loc, prop={'size': legend_size})
+    plt.legend(loc=legend_loc, prop={"size": legend_size})
     plt.xlabel("Month")
-    plt.ylabel("Monthly " + feature_to_compare);
+    plt.ylabel("Monthly " + feature_to_compare)
