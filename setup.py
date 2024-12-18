fdd10c5c37e362111d1ddcc688f9821de4b86d90
Aleksey Senkin <alexennk@yandex.by>
2024-12-18 15:56:25 +0300

    Production DS pt.1
    Changes:
    - added a setup.py file
    - added requirements.txt for all scripts/ necessary packages
    - adapted .gitignore for these changes

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+def read_requirements(filename):
+    return Path(filename).read_text().splitlines()
+
+
+test_deps = [
+    "pip>=24.3.0",
+    "flake8>=6.0.0",
+    "black>=22.6.0",
+]
+
+extras = {"test": test_deps}
+
+setup(
+    name="innowise-ml-internship-alexennk",
+    version="0.1.3",
+    author="Aleksey Senkin",
+    author_email="aleksey.senkin@innowise.com",
+    description="A package with scripts for ml-ds-project",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    # package_dir={"": "scripts"},
+    install_requires=read_requirements("requirements.txt"),
+    extras_require=extras,
+    python_requires=">=3.12",
+    include_package_data=True,
+    zip_safe=False,
+)
